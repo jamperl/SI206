44fadad8d3449ac007dcb606d38e3b0a55266b5e
jamperl <jamperl@umich.edu>
2016-11-09 15:52:47 -0500

    Re-commit of solutions after erasing

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -9,7 +9,53 @@
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
+
+import nltk
+import random
+from nltk.book import *
+from nltk import word_tokenize,sent_tokenize
+
 print("START*******")
 
+debug = False #True
+
+
+if debug:
+	print ("Getting information from file madlib_test.txt...\n")
+
+str_1 =  (text2[:151])
+param = ' '.join(str_1)
+print (param)
+tokens = nltk.word_tokenize(param)
+print("TOKENS")
+print(tokens)
+tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
+print("TAGGED TOKENS")
+print(tagged_tokens)
+if debug:
+	print ("First few tagged tokens are:")
+	for tup in tagged_tokens[:5]:
+		print (tup)
+
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective", "RB":"an adverb", "VBP":"a present tense verb"}
+substitution_probabilities = {"NN":.15,"NNS":.10,"VB":.10,"JJ":.10, "RB":.10, "VBP":.10}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
+
+
+for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+print ("".join(final_words))
 
 print("\n\nEND*******")
