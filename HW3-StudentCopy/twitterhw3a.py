6afc246f570f13d78f4e09bc9657d6275e6a1cdf
Jamie Perlmutter <jamieperlmutter@Jamies-MacBook-Pro.local>
2016-11-10 16:48:52 -0500

    Updates

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -16,6 +16,6 @@ auth.set_access_token(access_token,access_token_secret)
 
 api = tweepy.API(auth)
 
-img = 'leafs.png'
+img = 'media/leafs.jpg'
 txt = "#UMSI-206 #Proj3"
 api.update_with_media(img, status=txt)
